3a44d0a6f66f791eb5ee589339e0660e1c068889
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-09-16 11:11:58 -0700

    For feedback update

--- AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/components/fabric-feedback/fabric-feedback.component.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/components/fabric-feedback/fabric-feedback.component.ts
@@ -1,7 +1,9 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { PanelType } from 'office-ui-fabric-react';
-import { TelemetryService, TelemetryEventNames } from 'diagnostic-data';
+import { TelemetryService, TelemetryEventNames, ResourceDescriptor } from 'diagnostic-data';
 import { Globals } from '../../../globals';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'fabric-feedback',
@@ -13,33 +15,43 @@ export class FabricFeedbackComponent implements AfterViewInit {
   // dismissSubject: Subject<void> = new Subject<void>();
   ratingEventProperties: any;
   feedbackText: string = "";
-  feedbackIcons: {id:string,text:string}[] = 
-  [
+  feedbackIcons: { id: string, text: string }[] =
+    [
       {
-        id:"EmojiDisappointed",
-        text:"very dissatisfied"
-      }, 
-      {
-        id:"Sad",
-        text:"dissatisfied "
+        id: "EmojiDisappointed",
+        text: "very dissatisfied"
       },
       {
-        id:"EmojiNeutral",
-        text:"ok"
+        id: "Sad",
+        text: "dissatisfied "
       },
       {
-        id:"Emoji2",
-        text:"satisfied"
+        id: "EmojiNeutral",
+        text: "ok"
       },
       {
-        id:"Emoji",
-        text:"very satisfied"
+        id: "Emoji2",
+        text: "satisfied"
+      },
+      {
+        id: "Emoji",
+        text: "very satisfied"
       }
-  ];
+    ];
   submitted: boolean = false;
   rating: number = 0;
-  constructor(protected telemetryService: TelemetryService, public globals: Globals) {
-    // this.submitted = false;
+  feedbacks: Feedback[] = [];
+  resourceUri: string = "";
+  constructor(protected telemetryService: TelemetryService, public globals: Globals, private _http: HttpClient, private router: ActivatedRoute) {
+    // const resource = ResourceDescriptor.parseResourceUri(window.location.href);
+    const subscriptionid = this.router.snapshot.params["subscriptionid"];
+    const resourcegroup = this.router.snapshot.params["resourcegroup"];
+    const resourcename = this.router.snapshot.params["resourcename"];
+    this.resourceUri = `/subscriptions/${subscriptionid}/resourceGroups/${resourcegroup}/providers/Microsoft.Web/sites/${resourcename}`;
+    this.resourceUri = this.resourceUri.toLowerCase();
+    this.getFeedbacks().subscribe(feedbacks => {
+      this.feedbacks = feedbacks;
+    })
   }
 
   submitFeedback() {
@@ -53,6 +65,7 @@ export class FabricFeedbackComponent implements AfterViewInit {
       'Url': window.location.href
     };
     this.logEvent(TelemetryEventNames.StarRatingSubmitted, eventProps);
+    this.addFeedback(this.feedbackText);
     this.submitted = true;
   }
 
@@ -81,10 +94,10 @@ export class FabricFeedbackComponent implements AfterViewInit {
   ngAfterViewInit() {
     setTimeout(() => {
       const eles = document.querySelectorAll("#feedback-icons i");
-      if(eles && eles.length > 0) {
-        eles.forEach((ele,index) => {
-          ele.setAttribute("role","button");
-          ele.setAttribute("name",this.feedbackIcons[index].text);
+      if (eles && eles.length > 0) {
+        eles.forEach((ele, index) => {
+          ele.setAttribute("role", "button");
+          ele.setAttribute("name", this.feedbackIcons[index].text);
         });
       }
     });
@@ -101,4 +114,30 @@ export class FabricFeedbackComponent implements AfterViewInit {
     this.globals.openFeedback = false;
     this.reset();
   }
+
+  getFeedbacks() {
+    const url = "https://xiaoxu-func.azurewebsites.net/api/GetComments";
+    const data = { resourceuri: this.resourceUri };
+    return this._http.get<Feedback[]>(url, { params: data });
+  }
+
+  addFeedback(comment: string) {
+    if (comment === "") return;
+
+    const baseUrl = "https://xiaoxu-func.azurewebsites.net/api/AddComment";
+    let resourceuri = "/subscriptions/1402be24-4f35-4ab7-a212-2cd496ebdf14/resourceGroups/Default-Web-WestUS/providers/Microsoft.Web/sites/addingeventlogs";
+    resourceuri.toLowerCase();
+    const data = {
+      comment: comment,
+      author: "customer",
+      resourceuri: this.resourceUri
+    }
+    this._http.post(baseUrl, {}, { params: data }).subscribe(res => { });
+  }
+}
+
+interface Feedback {
+  comment: string;
+  author: string;
+  resourceuri?: string;
 }
